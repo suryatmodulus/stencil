bdaacf8a0b2f13c681e4bd0db7d2aee8647119b0
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-04-01 09:02:23 -0400

    refactor(docs): remove unused utilities
    this commit removes documentation generation utilities that are no
    longer used by the Stencil codebase

--- src/compiler/docs/readme/docs-util.ts
+++ src/compiler/docs/readme/docs-util.ts
@@ -1,5 +1,3 @@
-import type * as d from '../../../declarations';
-
 export class MarkdownTable {
   private rows: RowData[] = [];
 
@@ -41,7 +39,7 @@ const createTable = (rows: RowData[]) => {
     return content;
   }
 
-  normalizeColumCount(rows);
+  normalizeColumnCount(rows);
   normalizeColumnWidth(rows);
 
   const th = rows.find((r) => r.isHeader);
@@ -90,7 +88,7 @@ const createRow = (row: RowData) => {
   return content.join('').trim();
 };
 
-const normalizeColumCount = (rows: RowData[]) => {
+const normalizeColumnCount = (rows: RowData[]) => {
   let columnCount = 0;
 
   rows.forEach((r) => {
@@ -141,50 +139,3 @@ interface RowData {
   columns: ColumnData[];
   isHeader?: boolean;
 }
-
-export const getEventDetailType = (eventType: d.JsDoc) => {
-  if (eventType && eventType.type && typeof eventType.type === 'string' && eventType.type !== 'void') {
-    return eventType.type.trim();
-  }
-  return 'void';
-};
-
-export const getMemberDocumentation = (jsDoc: d.JsDoc) => {
-  if (jsDoc && typeof jsDoc.documentation === 'string') {
-    return jsDoc.documentation.trim();
-  }
-  return '';
-};
-
-export const getPlatform = (jsDoc: d.JsDoc) => {
-  const tag = jsDoc.tags.find((t) => t.name === 'platform');
-  return tag.text || 'all';
-};
-
-export const getMemberType = (jsDoc: d.JsDoc) => {
-  if (jsDoc && typeof jsDoc.type === 'string') {
-    return jsDoc.type.trim();
-  }
-  return '';
-};
-
-export const getMethodParameters = ({ parameters }: d.JsDoc): d.JsonDocMethodParameter[] => {
-  if (parameters) {
-    return parameters.map(({ name, type, documentation }) => ({
-      name,
-      type,
-      docs: documentation,
-    }));
-  }
-  return [];
-};
-
-export const getMethodReturns = ({ returns }: d.JsDoc): d.JsonDocsMethodReturn => {
-  if (returns) {
-    return {
-      type: returns.type,
-      docs: returns.documentation,
-    };
-  }
-  return null;
-};
